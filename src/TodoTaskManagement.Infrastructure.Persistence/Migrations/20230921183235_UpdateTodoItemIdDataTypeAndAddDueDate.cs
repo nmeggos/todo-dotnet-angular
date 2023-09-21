2982d7ab47060a314be42a3f247a1cb2a8bfcc52
Nikitas Meggos <meggosni@live.com>
2023-09-21 13:40:30 -0500

    Adding feature capability for completing a todo item Changed todo item data type to int from guid Added Due Date for todo item

---
+++ src/TodoTaskManagement.Infrastructure.Persistence/Migrations/20230921183235_UpdateTodoItemIdDataTypeAndAddDueDate.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TodoTaskManagement.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateTodoItemIdDataTypeAndAddDueDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsCompleted",
+                schema: "dbo",
+                table: "Todo");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                schema: "dbo",
+                table: "Todo",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier")
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DueDate",
+                schema: "dbo",
+                table: "Todo",
+                type: "datetime",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DueDate",
+                schema: "dbo",
+                table: "Todo");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                schema: "dbo",
+                table: "Todo",
+                type: "uniqueidentifier",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsCompleted",
+                schema: "dbo",
+                table: "Todo",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
